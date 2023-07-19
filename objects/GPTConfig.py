1ae183f0bfcd3fad65fa9d0bd13e166948cba876
Navid Rohim <navidrohim@Navids-MacBook-Pro.local>
2023-07-20 00:33:19 +0100

    Changed version number and set FINAL to True

--- objects/GPTConfig.py
+++ objects/GPTConfig.py
@@ -1,7 +1,7 @@
 import datetime as _datetime
 from discord import ActivityType
 
-FINAL = False
+FINAL = True
 DEBUG = True
 QUERY_TIMEOUT = 10
 QUERY_CONFIRMATION = ">y"
@@ -13,7 +13,7 @@ OPENAI_API_KEY = "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"
 STREAM_UPDATE_MESSAGE_FREQUENCY = 10
 CHATS_LIMIT = 20
 CHARACTER_LIMIT = 2000
-VERSION = "1.2.2"
+VERSION = "1.2.3"
 
 STATUS_TYPE = ActivityType.listening
 STATUS_TEXT = "/help AND answering lifes biggest questions."
