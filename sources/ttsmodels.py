1f8c1139ac9da91814d6f0f985f4c572e76b4afb
Navid Rohim <navidrohim@Navids-iMac.local>
2023-09-04 11:29:51 +0100

    Removed debug code

--- sources/ttsmodels.py
+++ sources/ttsmodels.py
@@ -2,7 +2,6 @@ import io as _io, gtts as _gtts, json as _json
 from .common.developerconfig import FFMPEG, FFPROBE
 from .voice import pydub as _pydub
 
-print("FFPATH ", FFMPEG)
 _pydub.AudioSegment.ffmpeg = FFMPEG
 
 """I want to put more TTS models here, but using one that is not system dependent and has a package for python is difficult."""
