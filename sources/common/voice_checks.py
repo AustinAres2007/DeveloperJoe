83678857eb2a59bde2d56b3eb8b20c5ddfc267ee
Navid <hakuares@gmail.com>
2023-09-10 17:24:06 +0100

    Version restictions to avoid errors

--- sources/common/voice_checks.py
+++ sources/common/voice_checks.py
@@ -1,22 +1,30 @@
 from sys import platform
 from pathlib import Path
-from platform import release, machine as arch
+from platform import release, machine 
 from warnings import warn
+
+arch = machine().lower()
 
 class OSTypes:
     MacOS = "darwin"
     Windows = "win32"
     Linux = "linux"
+    Unix = (Linux, MacOS)
     
 class Archs:
     arm64 = "arm64"
     x64 = ""
     
 def _get_path_according_to_specs(library: str) -> str:
-    if platform == OSTypes.Windows and arch().lower() == Archs.arm64:
-        warn("You are using an ARM64 Processor on a windows machine. This is not supported. Voice has been disabled.")
-        return ""
-    return str(Path("voice", platform, release() if platform == "win32" else platform, library).absolute())
+    if platform == OSTypes.Windows and arch == Archs.arm64:
+        warn("You are using an ARM64 processor on a Windows machine. This is not supported. Voice has been disabled.")
+    elif platform == OSTypes.MacOS and arch == Archs.x64:
+        warn("You are using an Intel processor on a Mac machine. This is not supported. Voice has been disabled.")
+    elif platform == OSTypes.Linux and arch == Archs.arm64:
+        warn("Somehow you are using an ARM64 processor on Linux. This is not supported. Voice has been disabled.")
+    else:
+        return str(Path("voice", platform, release() if platform == "win32" else platform, library).absolute())
+    return ""
 
 def _get_voice_paths(library: str, shared_lib: bool) -> str:
     executable_suffix = {
@@ -40,7 +48,9 @@ def _get_voice_paths(library: str, shared_lib: bool) -> str:
         return ""
 
 if __name__ == "__main__":
-    _get_voice_paths("ffmpeg", False)
-    _get_voice_paths("ffprobe", False)
-    _get_voice_paths("opus", True)
+    ffm = _get_voice_paths("ffmpeg", False)
+    ffp = _get_voice_paths("ffprobe", False)
+    opus = _get_voice_paths("opus", True)
+    
+    print(ffm, ffp, opus)
     
