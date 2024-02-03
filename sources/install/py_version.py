75841a5c3ed8f8fcfdd5f02bd22fc240f26a03ea
Navid <navidanjumsamirohim@gmail.com>
2024-02-03 10:01:10 -0800

    Windows script updated (Could be hard to read errors and less verbose output) and fixed install bug if on python 3.12

--- sources/install/py_version.py
+++ sources/install/py_version.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     from platform import python_version_tuple
     MAJOR, MINOR, PATCH = python_version_tuple()
     if int(MAJOR) >= 3:
-        if int(MINOR) == 11:
+        if int(MINOR) >= 12:
             exit(0)
     exit(1) # MUST BE EXIT CODE 1
